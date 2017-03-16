fdd45d103c7a73db07c5c6bb89036aed6b376ea2
Augusto <aluno@DCCLAB-CONV09>
2017-03-16 09:51:46 -0300

    Change project name

---
+++ src/br/ufla/dcc/masterchef/Usuario.java
@@ -0,0 +1,39 @@
+package br.ufla.dcc.masterchef;
+
+public class Usuario {
+
+    private String Nome;
+
+    private String Senha;
+
+    private String Login;
+
+    public Usuario Usuario() {
+        return null;
+    }
+
+    public void CadastrarReceita() {
+
+    }
+
+    public void RemoverReceita() {
+
+    }
+
+    public void EditarReceita() {
+
+    }
+
+    public void BuscarReceita() {
+
+    }
+
+    public void ListarReceitas() {
+
+    }
+
+    public void ExibirReceita() {
+
+    }
+
+}
