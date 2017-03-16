3058a15f667ae05fb6c8a6341b1972e7f479dcab
Augusto <aluno@DCCLAB-CONV09>
2017-03-16 13:55:27 -0300

    Salvar tudo 2

--- src/br/ufla/dcc/masterchef/Principal.java
+++ src/br/ufla/dcc/masterchef/Principal.java
@@ -15,7 +15,7 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        Usuario u1 = new Usuario("augusto", "1234");
+        Usuario u1 = new Usuario("Augusto Ferreira", "augusto", "1234");
 
         u1.CadastrarReceita();
 
